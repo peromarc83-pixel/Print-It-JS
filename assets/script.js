e2c5616424c69440fb1a57abf4e3ad005dbaa7f6
Marc Pero <peromarc83@gmail.com>
2026-03-10 09:39:54 +0100

    ajout du carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,51 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const ArrowLeft=document.querySelector("#arrow_left");
+const ArrowRight=document.querySelector("#arrow_right");
+const nbImg = document.querySelectorAll(".banner-img").length;
+const img = document.querySelectorAll(".banner-img");
+const bannerText = document.querySelector("#banner p");
+const dots = document.querySelectorAll(".dot");
+
+let i=0;
+
+ArrowRight.addEventListener('click',()=>{
+	if (i < nbImg -1)
+		i++;
+	else
+		i=0;
+	console.log(i);
+     updateBanner(i) 
+    });
+
+ArrowLeft.addEventListener('click',()=>{
+	if (i > 0)
+		i--;
+	else
+		i = nbImg -1;
+	console.log(i);
+	 updateBanner(i) 
+    });
+
+  function updateBanner(i) {
+   img.forEach((img, index) => {
+        img.style.transform = 'translateX(' + ((index - i) * 100) + '%)';
+    });
+
+	bannerText.innerHTML = slides[i].tagLine;
+
+
+  
+	dots.forEach((dot,index) => {
+        if (index === i) {
+            dot.style.backgroundColor = 'white';      // dot actif → visible
+        } else {
+            dot.style.backgroundColor = 'transparent';    // autres dots → transparents
+        }
+    });
+}
+  
+
+
